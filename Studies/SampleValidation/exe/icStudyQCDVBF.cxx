e41840c63a2065a55215f577b277e3ca9187853e
Joao Pela <pela@cern.ch>
2014-01-17 18:37:18 +0000

    End of the day commit 2014/01/17
    * Fixed weight calculation
    * Since ICTabular had his bug fixed, now producing table for xsec and weight per sample
    * Fixed some information processing

--- Studies/SampleValidation/exe/icStudyQCDVBF.cxx
+++ Studies/SampleValidation/exe/icStudyQCDVBF.cxx
@@ -72,11 +72,11 @@ int main(int argc, char *argv[]){
   evQCDInc["QCD-Pt-1800"]       = 947586.0;
     
   map<string,double> evQCDVBF;  
-  evQCDVBF["QCD_VBF-Pt-80to120"]  = 39376000000;
-  evQCDVBF["QCD_VBF-Pt-120to170"] = 7000000000;
-  evQCDVBF["QCD_VBF-Pt-170to300"] = 1375000000;
-  evQCDVBF["QCD_VBF-Pt-300to470"] = 80000000;
-  evQCDVBF["QCD_VBF-Pt-470to600"] = 25000000;
+  evQCDVBF["QCD-Pt-80to120"]  = 39376000000;
+  evQCDVBF["QCD-Pt-120to170"] = 7000000000;
+  evQCDVBF["QCD-Pt-170to300"] = 1375000000;
+  evQCDVBF["QCD-Pt-300to470"] = 80000000;
+  evQCDVBF["QCD-Pt-470to600"] = 25000000;
 
   vector<string> samples;
   samples.push_back("QCD-Pt-30to50");     
@@ -98,27 +98,26 @@ int main(int argc, char *argv[]){
   samples.push_back("QCD_VBF-Pt-470to600");
   
   map<string,double> wgt;
-  wgt["QCD-Pt-30to50"]     = (evQCDInc["QCD-Pt-30to50"]    *xsec["QCD-Pt-30to50"])    /lumi;
-  wgt["QCD-Pt-50to80"]     = (evQCDInc["QCD-Pt-50to80"]    *xsec["QCD-Pt-50to80"])    /lumi;
-  wgt["QCD-Pt-80to120"]    = (evQCDInc["QCD-Pt-80to120"]   *xsec["QCD-Pt-80to120"])   /lumi;
-  wgt["QCD-Pt-120to170"]   = (evQCDInc["QCD-Pt-120to170"]  *xsec["QCD-Pt-120to170"])  /lumi;
-  wgt["QCD-Pt-170to300"]   = (evQCDInc["QCD-Pt-170to300"]  *xsec["QCD-Pt-170to300"])  /lumi;
-  wgt["QCD-Pt-300to470"]   = (evQCDInc["QCD-Pt-300to470"]  *xsec["QCD-Pt-300to470"])  /lumi;
-  wgt["QCD-Pt-470to600"]   = (evQCDInc["QCD-Pt-470to600"]  *xsec["QCD-Pt-470to600"])  /lumi;
-  wgt["QCD-Pt-600to800"]   = (evQCDInc["QCD-Pt-600to800"]  *xsec["QCD-Pt-600to800"])  /lumi;
-  wgt["QCD-Pt-800to1000"]  = (evQCDInc["QCD-Pt-800to1000"] *xsec["QCD-Pt-800to1000"]) /lumi;
-  wgt["QCD-Pt-1000to1400"] = (evQCDInc["QCD-Pt-1000to1400"]*xsec["QCD-Pt-1000to1400"])/lumi;
-  wgt["QCD-Pt-1400to1800"] = (evQCDInc["QCD-Pt-1400to1800"]*xsec["QCD-Pt-1400to1800"])/lumi;
-  wgt["QCD-Pt-1800"]       = (evQCDInc["QCD-Pt-1800"]      *xsec["QCD-Pt-1800"])      /lumi;
-
-  wgt["QCD_VBF-Pt-80to120"]  = (evQCDInc["QCD_VBF-Pt-80to120"] *xsec["QCD-Pt-80to120"]) /lumi;
-  wgt["QCD_VBF-Pt-120to170"] = (evQCDInc["QCD_VBF-Pt-120to170"]*xsec["QCD-Pt-120to170"])/lumi;
-  wgt["QCD_VBF-Pt-170to300"] = (evQCDInc["QCD_VBF-Pt-170to300"]*xsec["QCD-Pt-170to300"])/lumi;
-  wgt["QCD_VBF-Pt-300to470"] = (evQCDInc["QCD_VBF-Pt-300to470"]*xsec["QCD-Pt-300to470"])/lumi;
-  wgt["QCD_VBF-Pt-470to600"] = (evQCDInc["QCD_VBF-Pt-470to600"]*xsec["QCD-Pt-470to600"])/lumi;
+  wgt["QCD-Pt-30to50"]       = (lumi*xsec["QCD-Pt-30to50"])    /evQCDInc["QCD-Pt-30to50"]    ;
+  wgt["QCD-Pt-50to80"]       = (lumi*xsec["QCD-Pt-50to80"])    /evQCDInc["QCD-Pt-50to80"]    ;
+  wgt["QCD-Pt-80to120"]      = (lumi*xsec["QCD-Pt-80to120"])   /evQCDInc["QCD-Pt-80to120"]   ;
+  wgt["QCD-Pt-120to170"]     = (lumi*xsec["QCD-Pt-120to170"])  /evQCDInc["QCD-Pt-120to170"]  ;
+  wgt["QCD-Pt-170to300"]     = (lumi*xsec["QCD-Pt-170to300"])  /evQCDInc["QCD-Pt-170to300"]  ;
+  wgt["QCD-Pt-300to470"]     = (lumi*xsec["QCD-Pt-300to470"])  /evQCDInc["QCD-Pt-300to470"]  ;
+  wgt["QCD-Pt-470to600"]     = (lumi*xsec["QCD-Pt-470to600"])  /evQCDInc["QCD-Pt-470to600"]  ;
+  wgt["QCD-Pt-600to800"]     = (lumi*xsec["QCD-Pt-600to800"])  /evQCDInc["QCD-Pt-600to800"]  ;
+  wgt["QCD-Pt-800to1000"]    = (lumi*xsec["QCD-Pt-800to1000"]) /evQCDInc["QCD-Pt-800to1000"] ;
+  wgt["QCD-Pt-1000to1400"]   = (lumi*xsec["QCD-Pt-1000to1400"])/evQCDInc["QCD-Pt-1000to1400"];
+  wgt["QCD-Pt-1400to1800"]   = (lumi*xsec["QCD-Pt-1400to1800"])/evQCDInc["QCD-Pt-1400to1800"];
+  wgt["QCD-Pt-1800"]         = (lumi*xsec["QCD-Pt-1800"])      /evQCDInc["QCD-Pt-1800"]      ;
+  wgt["QCD_VBF-Pt-80to120"]  = (lumi*xsec["QCD-Pt-80to120"])   /evQCDVBF["QCD-Pt-80to120"]   ;
+  wgt["QCD_VBF-Pt-120to170"] = (lumi*xsec["QCD-Pt-120to170"])  /evQCDVBF["QCD-Pt-120to170"]  ;
+  wgt["QCD_VBF-Pt-170to300"] = (lumi*xsec["QCD-Pt-170to300"])  /evQCDVBF["QCD-Pt-170to300"]  ;
+  wgt["QCD_VBF-Pt-300to470"] = (lumi*xsec["QCD-Pt-300to470"])  /evQCDVBF["QCD-Pt-300to470"]  ;
+  wgt["QCD_VBF-Pt-470to600"] = (lumi*xsec["QCD-Pt-470to600"])  /evQCDVBF["QCD-Pt-470to600"]  ;
   
   //_________________________________________  
-  ICLatexTabular tabXSec(19,3);
+  ICLatexTabular tabXSec(18,3);
   
   tabXSec.set(0,0,"Sample");
   tabXSec.set(0,1,"Cross Section [pb]");
@@ -127,38 +126,58 @@ int main(int argc, char *argv[]){
   cout << "N Samples: " << samples.size() << endl;
   for(unsigned i=0; i<samples.size(); i++){
     
-//     string* s = &(samples[i]);
+    string* s = &(samples[i]);
     
-//     tabXSec.set(int(i+1),0,samples[i]);//samples[i]);  
-//     tabXSec.set(int(i+1),1,"");//xsec[(*s)]);
-//     tabXSec.set(int(i+1),2,"");//wgt [(*s)]);
+    tabXSec.set(int(i+1),0,(*s));//samples[i]);  
+    //tabXSec.set(int(i+1),1,xsec[(*s)]);//xsec[(*s)]);
+    //tabXSec.set(int(i+1),2,wgt [(*s)]);//wgt [(*s)]);
   }
+    
+  tabXSec.setColumnDecorationBefore(0,"|");
+  tabXSec.setColumnDecorationAfter (2,"|");
   
-//   tabXSec.setColumnDecorationBefore(0,"|");
-//   tabXSec.setColumnDecorationAfter(2,"|");
+  tabXSec.setRowDecorationBefore( 0,"\\hline");
+  tabXSec.setRowDecorationBefore( 1,"\\hline \\hline");
+  tabXSec.setRowDecorationAfter (17,"\\hline");
   
-//   tabXSec.setRowDecorationBefore( 0,"\\hline");
-//   tabXSec.setRowDecorationBefore( 1,"\\hline \\hline");
-//   tabXSec.setRowDecorationAfter (18,"\\hline");
-  
-//   tabXSec.saveAs("table_xSec.tex");
+  tabXSec.saveAs("table_xSec.tex");
   
   //_________________________________________
+  cout << "Weights" << endl;
+  for(unsigned i=0; i<samples.size() ; i++){
+    
+    string s = samples[i];
+    double w = wgt[s];
+    
+    printf("%20s : %10.4f\n",s.c_str(),w);
+  }
+  cout<<endl;
+    
+  //_________________________________________    
   map<string,TH1F*> JetPair_n_vtx;
-  
   for(map<string,TFile*>::iterator f=files.begin(); f!=files.end(); f++){
     
     TH1F* h = (TH1F*) f->second->Get("JetPair/n_vtx");
     
     JetPair_n_vtx[f->first] = h;
-    double entries  = h->GetEntries();
-    double integral = h->Integral(0,h->GetNbinsX()+1);
-    double intWgt   = integral*wgt[f->first];
-    
-    printf("%20s : %8.0f : %10.2f : %10.2f\n",(f->first).c_str(),entries,integral,intWgt);
     
   }
-  
+  cout<<endl;  
+
+  //_________________________________________  
+  cout << "Entries" << endl;
+  for(unsigned i=0; i<samples.size() ; i++){
+    
+    string s=samples[i];
+    TH1F* h = JetPair_n_vtx[s];
+
+    double entries  = h->GetEntries();
+    double integral = h->Integral(0,h->GetNbinsX()+1);
+    double intWgt   = integral*wgt[s];
+    
+    printf("%20s : %8.0f : %10.2f : %10.2f\n",s.c_str(),entries,integral,intWgt);    
+  } 
+
   return 0;
 
 }
